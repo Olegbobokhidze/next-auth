dea44c2cd071261d102e3c14492bd6606b22fc04
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2024-07-17 15:39:25 +0400

    wip: add login-form components

---
+++ components/auth/back-button.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+interface BackButtonProps {
+  label: string;
+  href: string;
+}
+
+export const BackButton: React.FC<BackButtonProps> = ({ label, href }) => {
+  return (
+    <Button variant="link" className="font-normal w-full" size="sm" asChild>
+      <Link href={href}>{label}</Link>
+    </Button>
+  );
+};
---
+++ components/auth/header.tsx
@@ -0,0 +1,21 @@
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+interface HeaderProps {
+  label: string;
+}
+
+export const Header: React.FC<HeaderProps> = ({ label }) => {
+  return (
+    <div className="flex flex-col gap-y-4 items-center justify-center w-full">
+      <h1 className={cn("text-3xl font-semibold", font.className)}>Auth</h1>
+      <p className="text-muted-foreground text-sm">{label}</p>
+    </div>
+  );
+};
